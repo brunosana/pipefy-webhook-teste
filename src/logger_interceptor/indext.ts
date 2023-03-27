df418f61e20b6b1486ff9b17a52681e9b01566d9
brunosana <brunosanadev@gmail.com>
2023-03-27 17:40:45 -0300

    fix: request

--- src/logger_interceptor/indext.ts
+++ src/logger_interceptor/indext.ts
@@ -4,6 +4,7 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { Observable, tap } from 'rxjs';
 
 @Injectable()
@@ -11,10 +12,10 @@ export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log('Before...');
     const request = context.switchToHttp().getRequest<Request>();
-    console.log('request: ');
-    request.json().then((json) => {
-      console.log(JSON.stringify(json));
-    });
+    console.log('body', request.body);
+    console.log('headers', request.headers);
+    console.log('params', request.params);
+    console.log('query', request.query);
     const now = Date.now();
     return next
       .handle()
